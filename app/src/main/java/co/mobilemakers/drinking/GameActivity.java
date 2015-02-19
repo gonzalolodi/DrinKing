8b634531ab6e45f6452e3905eb84232a17f15215
Gonzalo Lodi <gonzalo.lodi@globant.com>
2015-02-19 17:26:48 -0300

    Style set, buttons edited.

--- app/src/main/java/co/mobilemakers/drinking/GameActivity.java
+++ app/src/main/java/co/mobilemakers/drinking/GameActivity.java
@@ -3,22 +3,17 @@ package co.mobilemakers.drinking;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 
-public class StartActivity extends ActionBarActivity {
+public class GameActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_game);
         customizeActionBar();
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
